3897de5e4df07b148fb45c97215add1b50558581
Denrubka <74959243+Denrubka@users.noreply.github.com>
2021-04-25 16:46:29 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const navList = document.querySelector('.nav-list');
+  const openNav = () => {
+    burger.addEventListener('click', () => {
+      navList.classList.toggle('show');
+    });
+  }
+  openNav();
+});
